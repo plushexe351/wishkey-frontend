ee9d09dd0155d8595eff067c17a348ee857a0f5e
plushexe351 <geekyushnish@gmail.com>
2025-04-23 01:06:50 +0530

    update hardcoded profile images in MyDreamsMenu and fix overflow

--- src/components/MyDreamsMenu.jsx
+++ src/components/MyDreamsMenu.jsx
@@ -1,7 +1,15 @@
 import { Edit2, Plus } from "react-feather";
 import pfp from "../assets/pfp.jpeg";
+import pfp2 from "../assets/pfp2.jpeg";
+import pfp3 from "../assets/pfp3.jpeg";
+import pfp4 from "../assets/pfp4.jpeg";
 
 const MyDreamsMenu = ({ blur }) => {
+  const categories = [
+    { name: "X-mas", items: 4 },
+    { name: "Happy Birthday", items: 3 },
+    { name: "Happy Birthday long category name", items: 3 },
+  ];
   return (
     <div className={`menu ${blur ? "blur" : ""}`}>
       <div className="row all-dreams">
@@ -19,7 +27,14 @@ const MyDreamsMenu = ({ blur }) => {
             </div>
           </div>
 
-          <div className="category">
+          {categories.map((category, index) => (
+            <div className="category" key={index}>
+              <div className="category-title">{category.name}</div>
+              <div className="items-in-category">{category.items}</div>
+            </div>
+          ))}
+
+          {/* <div className="category">
             <div className="category-title">X-mas</div>
             <div className="items-in-category">4</div>
           </div>
@@ -32,7 +47,7 @@ const MyDreamsMenu = ({ blur }) => {
               Happy Birthday long category name
             </div>
             <div className="items-in-category">3</div>
-          </div>
+          </div> */}
         </div>
         <div className="friends">
           <div className="friends-title">Friends</div>
@@ -40,9 +55,9 @@ const MyDreamsMenu = ({ blur }) => {
             <div className="number-of-friends">21</div>
             <div className="friends-list">
               <img src={pfp} alt="friend" className="friend-image" />
-              <img src={pfp} alt="friend" className="friend-image" />
-              <img src={pfp} alt="friend" className="friend-image" />
-              <img src={pfp} alt="friend" className="friend-image" />
+              <img src={pfp2} alt="friend" className="friend-image" />
+              <img src={pfp3} alt="friend" className="friend-image" />
+              <img src={pfp4} alt="friend" className="friend-image" />
             </div>
           </div>
         </div>
