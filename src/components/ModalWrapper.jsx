05f676b856933f241b3bbe376922fd06ef295088
plushexe351 <geekyushnish@gmail.com>
2025-04-23 05:18:59 +0530

    update modal position

--- src/components/ModalWrapper.jsx
+++ src/components/ModalWrapper.jsx
@@ -16,9 +16,9 @@ const ModalWrapper = ({ children, name }) => {
       }}
     >
       <motion.div
-        initial={{ y: 100 }}
-        animate={{ y: 0 }}
-        exit={{ y: 100 }}
+        initial={{ scale: 0.9 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0.9 }}
         transition={{ duration: 0.2 }}
         className={`modal-content ${name}`}
         onClick={(e) => {
